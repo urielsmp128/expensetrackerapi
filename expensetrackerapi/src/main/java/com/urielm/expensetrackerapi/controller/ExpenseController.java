fe365c1f7fc75f3c860675f9762fa96b6c55f789
urielsmp128 <urielsmp128@outlook.com>
2024-09-13 22:31:23 -0600

    Establishing the connection with the database

--- expensetrackerapi/src/main/java/com/urielm/expensetrackerapi/controller/ExpenseController.java
+++ expensetrackerapi/src/main/java/com/urielm/expensetrackerapi/controller/ExpenseController.java
@@ -1,10 +1,11 @@
 package com.urielm.expensetrackerapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExpenseController {
-
+    @GetMapping("/expenses")
     public String getAllExpenses(){
         return "List of all expenses";
     }
